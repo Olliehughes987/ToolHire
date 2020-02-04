4cd726aa244f0d3bb23ddc7f1b1fa1c99532552b
Oliver Hughes <P2421386@my365.dmu.ac.uk>
2020-02-04 15:47:09 +0000

    added test class for orders

---
+++ ToolHireTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ToolHireTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+
+            clsOrders AnOrder = new cldOrders();
+            Assert.IsNotNull(AnOrder);
+        }
+    }
+}
