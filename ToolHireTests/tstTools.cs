5fad1df06b0547e182dc42ae2532fd3d9beaf1ac
Hayyan Hussain <P2508339@my365.dmu.ac.uk>
2020-02-04 16:49:10 +0000

    Added testing anc class for tools.

---
+++ ToolHireTests/tstTools.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToolHireClasses;
+
+namespace ToolHireTests
+{
+    [TestClass]
+    public class tstTools
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsTools AnTools = new clsTools();
+            Assert.IsNotNull(AnTools);
+
+        }
+    }
+
+}
