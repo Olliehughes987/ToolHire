37e43c050d195f578cbcbe352270c75669bb2a38
p2420272 <p2420272.my365.dmu.ac.uk>
2020-02-11 15:11:19 +0000

    Added test class for customer

---
+++ ToolHireTests/tstCustomer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ToolHireTests
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //create an instance of the class we want to create
+            clsCustomer anCustomer = new clsCustomer();
+            //test to see that it exists
+            Assert.IsNotNull(anCustomer);
+        }
+    }
+}
